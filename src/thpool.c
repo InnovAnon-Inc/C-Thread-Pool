ecb0d7bcb4683530a741c3054d8a7dc86be6d12f
Default User <laurence.a.maddox@gmail.com>
2017-09-08 06:01:24 -0500

    added warning flags

--- src/thpool.c
+++ src/thpool.c
@@ -146,7 +146,7 @@ struct thpool_* thpool_init(int num_threads){
 	}
 
 	/* Make threads in pool */
-	thpool_p->threads = (struct thread**)malloc(num_threads * sizeof(struct thread *));
+	thpool_p->threads = (struct thread**)malloc((size_t) num_threads * sizeof(struct thread *));
 	if (thpool_p->threads == NULL){
 		err("thpool_init(): Could not allocate memory for threads\n");
 		jobqueue_destroy(&thpool_p->jobqueue);
